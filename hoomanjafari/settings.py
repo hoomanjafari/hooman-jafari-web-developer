ee5f787fbd99fa56f6e58c99df1330c486fbdd54
hoomanjafari <135450451+hoomanjafari@users.noreply.github.com>
2024-11-08 23:00:09 +0330

    Update settings.py
    some security added

--- hoomanjafari/settings.py
+++ hoomanjafari/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wgkm1pduf@lui=p+h_p=do7&5itif)0l6&0)f9g&xf%izjovjg'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
